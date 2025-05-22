7c5dc7ce7d7a038c3b96f555b8505444bb67d81e
esipov@sv-en.ru <esipov@sv-en.ru>
2025-05-22 15:16:09 +0300

    запросы с GET изменены на POST

--- src/ntuRouter.js
+++ src/ntuRouter.js
@@ -5,17 +5,17 @@ import writeToFile from './writeLog.js';
 
 const router = express.Router();
 
-router.get('/ntuStatus', async (req, res) => {
+router.post('/ntuStatus', async (req, res) => {
     await writeToFile('Вызван маршрут: /ntuStatus');
     ntuController.getStatusNtu(req, res, 'ntuStatus');
 });
 
-router.get('/ntuStatusList', async (req, res) => {
+router.post('/ntuStatusList', async (req, res) => {
     await writeToFile('Вызван маршрут: /ntuStatusList');
     ntuController.getStatusNtu(req, res, 'ntuStatusList');
 });
 
-router.get('/readLog', async (req, res) => {
+router.post('/readLog', async (req, res) => {
     await writeToFile('Вызван маршрут: /readLog');
     fileController.getFileLog(req, res, 'ntuStatusList');
 });
